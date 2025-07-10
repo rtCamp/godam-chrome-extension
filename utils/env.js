645f8ad672cf2406201c39da5ac1e13ec315c3dd
Ayush Nirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-07-10 15:26:32 +0530

    Fix: build routine (#26)
    * chore: add prod build command
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: update defaults in env.js
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: load env vars from env.js
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- utils/env.js
+++ utils/env.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 // tiny wrapper with default env vars
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
+  BABEL_ENV: process.env.BABEL_ENV || 'development',
   PORT: process.env.PORT || 3000,
   GODAM_BASE_URL: process.env.GODAM_BASE_URL || 'https://app-godam.rt.gw',
   GODAM_UPLOAD_URL: process.env.GODAM_UPLOAD_URL || 'https://godam-upload.rt.gw',
   GODAM_OAUTH_CLIENT_ID: process.env.GODAM_OAUTH_CLIENT_ID || 'habg22ul6k',
+  ASSET_PATH : process.env.ASSET_PATH || "/",
 };
