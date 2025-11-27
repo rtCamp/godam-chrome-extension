57fd5c64bb05cb38973a1a6f252ff8b67ca91da0
robinroy03 <robinroy.work@gmail.com>
2025-11-27 10:48:17 +0530

    2 space indentation

--- src/utils/editor/generateThumbstrips.js
+++ src/utils/editor/generateThumbstrips.js
@@ -1,45 +1,45 @@
 /* generateThumbstrips.js */
 async function generateThumbstrips(videoBlob, ffmpeg, frames = 5) {
-    // Read video data
-    const videoData = new Uint8Array(await videoBlob.arrayBuffer());
+  // Read video data
+  const videoData = new Uint8Array(await videoBlob.arrayBuffer());
 
-    // Write video data to the virtual file system
-    ffmpeg.FS("writeFile", "input.mp4", videoData);
+  // Write video data to the virtual file system
+  ffmpeg.FS("writeFile", "input.mp4", videoData);
 
-    const thumbstrips = [];
+  const thumbstrips = [];
 
-    const duration = ffmpeg.getInfo("duration");
-    const interval = duration / frames;
+  const duration = ffmpeg.getInfo("duration");
+  const interval = duration / frames;
 
-    for (let i = 0; i < frames; i++) {
-        const time = i * interval;
-        const outputFileName = `output${i.toString().padStart(3, "0")}.jpg`;
+  for (let i = 0; i < frames; i++) {
+    const time = i * interval;
+    const outputFileName = `output${i.toString().padStart(3, "0")}.jpg`;
 
-        // Use FFmpeg to extract a frame as a JPEG image
-        await ffmpeg.run(
-            "-i",
-            "input.mp4",
-            "-ss",
-            time.toString(),
-            "-frames:v",
-            "1",
-            "-q:v",
-            "3",
-            outputFileName
-        );
+    // Use FFmpeg to extract a frame as a JPEG image
+    await ffmpeg.run(
+      "-i",
+      "input.mp4",
+      "-ss",
+      time.toString(),
+      "-frames:v",
+      "1",
+      "-q:v",
+      "3",
+      outputFileName
+    );
 
-        // Read the generated thumbnail image
-        const thumbnailData = ffmpeg.FS("readFile", outputFileName);
+    // Read the generated thumbnail image
+    const thumbnailData = ffmpeg.FS("readFile", outputFileName);
 
-        // Create a Blob from the thumbnail data
-        const thumbnailBlob = new Blob([thumbnailData.buffer], {
-            type: "image/jpeg",
-        });
+    // Create a Blob from the thumbnail data
+    const thumbnailBlob = new Blob([thumbnailData.buffer], {
+      type: "image/jpeg",
+    });
 
-        thumbstrips.push(thumbnailBlob);
-    }
+    thumbstrips.push(thumbnailBlob);
+  }
 
-    return thumbstrips;
+  return thumbstrips;
 }
 
 export default generateThumbstrips;
